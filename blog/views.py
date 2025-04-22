bb7c8d68b30ed92a6c72337d0bf4eb04e6a6725b
mittal.nishchay1501@gmail.com <mittal.nishchay1501@gmail.com>
2025-04-23 02:30:57 +0530

    Individual Pages

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,  get_object_or_404
 from django.utils import timezone
 from .models import Post
 
@@ -7,3 +7,6 @@ def post_list(request):
     posts = Post.objects.all()
     return render(request, 'blog/post_list.html', {'posts': posts})
 
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)  # Get post or show 404 if not found
+    return render(request, 'blog/post_detail.html', {'post': post})
